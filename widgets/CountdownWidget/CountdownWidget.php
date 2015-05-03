b5b2c6a722695310751531458260092e7aa0dadf
Maksim Grishanov <max100491@mail.ru>
2015-05-04 00:15:11 +0500

    Создал модель, дописал виджет

--- widgets/CountdownWidget/CountdownWidget.php
+++ widgets/CountdownWidget/CountdownWidget.php
@@ -5,7 +5,9 @@
 class CountdownWidget extends \yupe\widgets\YWidget
 {
     public $id = 'retroclockbox';
+    public $model_id;
     public $options = [];
+
     public function init()
     {
         $assets = Yii::app()->assetManager->publish(
@@ -19,6 +21,12 @@ class CountdownWidget extends \yupe\widgets\YWidget
                 $('#".$this->id."').flipcountdown(".CJavaScript::encode($this->options).");
             })
         ");
+
+        if (isset($this->model_id) && is_int($this->model_id)) {
+            $model = Countdown::model()->findByPk($this->model_id);
+            if ($model===null)
+                throw new CException('В базе нет такой записи');
+        }
         parent::init();
     }
     public function run()
